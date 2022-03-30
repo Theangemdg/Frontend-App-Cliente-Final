565bef84a3cd7689da7e29be81fa588da5f21f72
Theangemdg <danielsanchez@unah.hn>
2022-03-29 23:17:36 -0600

    diseño Login

---
+++ js/controlador.js
@@ -0,0 +1,80 @@
+var usuarios = [
+    {
+        nombre: "Pedro",
+        correo: "Martinez",
+        contraseña: "as123",
+        ubicacion: "col-flor",
+        ordenes: [
+            {
+                nombreProducto: "Producto 1",
+                descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, modi!",
+                cantidad: 3,
+                precio: 49.99
+            },
+            {
+                nombreProducto: "Producto 2",
+                descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, modi!",
+                cantidad: 1,
+                precio: 49.99
+            }
+        ],
+        metodoPago: [{
+            nombrePago: "Pedro Martinez",
+            numeroTargeta: "123456",
+            FechaExpiracion: "22/02/2022",
+            CVV: "756",
+        }]
+    },
+    {
+        nombre: "Juan",
+        correo: "Perez",
+        contraseña: "as123",
+        ubicacion: "col-flor",
+        ordenes: [
+            {
+                nombreProducto: "Producto 3",
+                descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, modi!",
+                cantidad: 2,
+                precio: 49.99
+            },
+            {
+                nombreProducto: "Producto 2",
+                descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, modi!",
+                cantidad: 4,
+                precio: 49.99
+            }
+        ],
+        metodoPago: [{
+            nombrePago: "Juan Perez",
+            numeroTargeta: "123456",
+            FechaExpiracion: "22/02/2022",
+            CVV: "756",
+        }]
+    },
+    {
+        nombre: "Maria",
+        correo: "Rodriguez",
+        contraseña: "as123",
+        ubicacion: "col-flor",
+        ordenes: [
+            {
+                nombreProducto: "Producto 4",
+                descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, modi!",
+                cantidad: 5,
+                precio: 49.99
+            },
+            {
+                nombreProducto: "Producto 1",
+                descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, modi!",
+                cantidad: 4,
+                precio: 49.99
+            }
+        ],
+        metodoPago: [{
+            nombrePago: "Maria Rodriguez",
+            numeroTargeta: "123456",
+            FechaExpiracion: "22/02/2022",
+            CVV: "756",
+        }]
+    }
+];
