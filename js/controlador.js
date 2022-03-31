60aec06487e9d24398effa2336a88e9d926533f8
Theangemdg <danielsanchez@unah.hn>
2022-03-30 22:57:32 -0600

    funcionalidad aun no terminada

--- js/controlador.js
+++ js/controlador.js
@@ -108,5 +108,51 @@ function agregarUsuario() {
     console.log(usuario)
     usuariosPortal.push(usuario);
     localStorage.setItem('usuarios', JSON.stringify(usuariosPortal));
+}
 
+function validarUsuario(correo,contraseña){
+    let txtCorreo = document.getElementById('txt-correo').value;
+    let txtContraseña = document.getElementById('txt-contraseña').value;
+    var bAcces = false; 
+
+    for(let i=0; i<usuariosPortal.length; i++){
+        if(txtCorreo == usuariosPortal[i].correo && txtContraseña == usuariosPortal[i].contraseña){
+            bAcces = true;
+            sessionStorage.setItem('Usuario activo', usuariosPortal[i].nombre);
+        }
+    }
+
+    return bAcces;
+}
+
+function ingresar(){
+    var ucorreo = '';
+    var contraseña = '';
+    var bAcces = false;
+
+    ucorreo = document.getElementById('txt-correo').value;
+    contraseña = document.getElementById('txt-contraseña').value;
+    
+    bAcces = validarUsuario(ucorreo,contraseña);
+}
+
+function iniciarUsuario(){
+    document.getElementById('contenido-page').classList.toggle('d-none');
+    document.getElementById('menu-lateral').classList.toggle('d-none');
+    document.getElementById('cont-menu').style.backgroundColor = 'rgba(68, 186, 230, 1)';
+
+    document.getElementById('menu-lateral').innerHTML = 
+    `
+    <div class="container contenedor-1">
+        <div style="margin-top: 40px;">
+            <img id="profile-photo" src="img/default-profile.jpg" class="rounded-circle position-img"alt="">
+            <a id="inicarss" href="#">Iniciar Sesion</a>
+        </div>
+    </div>
+    <div class="container contenedor-2">
+        <button type="button">Carrito</button>
+        <button type="button">Cerrar Sesion</button>
+        <button type="button">Estado de las ordenes</button>
+    </div>
+    `
 }
